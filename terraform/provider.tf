a7107c27825ddc9fbe158ab69e02518a72e4ca27
PRASHANTsINHA13 <73397037+PRASHANTsINHA13@users.noreply.github.com>
2024-05-18 18:39:37 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,10 @@
 locals {
   region = "us-east-1"
-  name   = "amonkincloud-cluster"
+  name   = "EKS-terraform"
   vpc_cidr = "10.0.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
+  private_subnets = ["10.0.144.0/20", "10.0.128.0/20"]
   tags = {
     Example = local.name
   }
