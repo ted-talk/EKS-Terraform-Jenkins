d9b5ad574d112a5f6963f106034a168158514a19
PRASHANTsINHA13 <73397037+PRASHANTsINHA13@users.noreply.github.com>
2024-05-18 18:36:47 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 locals {
   region = "us-east-1"
   name   = "amonkincloud-cluster"
-  vpc_cidr = "10.123.0.0/16"
+  vpc_cidr = "10.0.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
