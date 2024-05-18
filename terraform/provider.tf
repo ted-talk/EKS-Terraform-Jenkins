a82daffde658dea9ab0979686aec67569666d33c
PRASHANTsINHA13 <73397037+PRASHANTsINHA13@users.noreply.github.com>
2024-05-18 18:45:29 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,7 @@ locals {
   azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
   private_subnets = ["10.0.144.0/20", "10.0.128.0/20"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
   tags = {
     Example = local.name
   }
